5f8633ad9d266bcf1d07f0c588977494a62a6b5d
Komil Kurb <komilpilot@gmail.com>
2025-11-29 21:21:41 -0500

    Auto: update Codewars solutions

--- 8kyu/multiply.js
+++ 8kyu/multiply.js
@@ -1,6 +1,19 @@
 // 8kyu - Multiply
 // https://www.codewars.com/kata/50654ddff44f800200000004
 
-function multiply(addEventListener, b){
+// 1)Function Declaration
+function multiply(a, b){
     return a * b;
 }
+
+// 2)Function Expression
+const multiply = function (a, b) {
+  return a * b;
+};
+
+
+// 3)Arrow function
+const multiply = (a, b) => a * b;
+
+console.log(multiply(5, 7)); // 35
+console.log(multiply(23, 71)); // 1633
