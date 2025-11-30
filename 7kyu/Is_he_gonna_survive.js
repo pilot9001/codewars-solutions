535625eb037bc4bbaef35b0e35fd934a1b118cf1
Komil Kurb <komilpilot@gmail.com>
2025-11-30 07:34:45 -0500

    Auto: update Codewars solutions

---
+++ 7kyu/Is_he_gonna_survive.js
@@ -0,0 +1,19 @@
+function hero(bullets, dragons){
+  return bullets >= dragons * 2;
+}
+
+function hero(bullets, dragons){
+    // Each dragon needs 2 bullets
+    let bulletsNedeed = dragons * 2;
+
+    // Check if the hero has enough bullets
+    if(bullets >= bulletsNedeed){
+        return true; // He survives 
+    } else {
+        return false; // He doesn't survive
+    }
+}
+
+console.log(hero(10, 5));
+console.log(hero(7, 4));
+console.log(hero(3, 2));
--- 8kyu/multiply.js
+++ 8kyu/multiply.js
@@ -1,19 +1,9 @@
-// 8kyu - Multiply
-// https://www.codewars.com/kata/50654ddff44f800200000004
-
-// 1)Function Declaration
 function multiply(a, b){
-    return a * b;
-}
-
-// 2)Function Expression
-const multiply = function (a, b) {
+  if (!a || !b || typeof(a) != "number" || typeof(b) != "number") {
+    return 0;
+  }
   return a * b;
-};
-
-
-// 3)Arrow function
-const multiply = (a, b) => a * b;
+}
 
 console.log(multiply(5, 7)); // 35
 console.log(multiply(23, 71)); // 1633
