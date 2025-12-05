847eb969b2c7eaea9e4abee4ede4c05c51da0b98
Komil Kurb <komilpilot@gmail.com>
2025-12-05 10:52:00 -0500

    Auto: update Codewars solutions

--- 8kyu/ArrayPlusArray.js
+++ 8kyu/ArrayPlusArray.js
@@ -12,4 +12,4 @@ function arrayPlusArray(arr1, arr2){
     return sum; // return the total 
 }
 
-console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])); //output: 21
