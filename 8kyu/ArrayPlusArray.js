9657e0de23ce5e1c8ac2a96fae7cb0584d766abb
Komil Kurb <komilpilot@gmail.com>
2025-12-06 08:52:48 -0500

    Auto: update Codewars solutions

--- 8kyu/ArrayPlusArray.js
+++ 8kyu/ArrayPlusArray.js
@@ -1,15 +1,64 @@
-function arrayPlusArray(arr1, arr2){
-    let sum = 0; // start from 0
+// // function arrayPlusArray(arr1, arr2){
+// //     let sum = 0; // start from 0
 
-    // add all numbers from first array
-    for(let i = 0; i < arr1.length; i++){
-        sum += arr1[i];
-    }
-    // add all numbers from second array
-    for(let i = 0; i < arr2.length; i++){
-        sum += arr2[i];
-    }
-    return sum; // return the total 
+// //     // add all numbers from first array
+// //     for(let i = 0; i < arr1.length; i++){
+// //         sum += arr1[i];
+// //     }
+// //     // add all numbers from second array
+// //     for(let i = 0; i < arr2.length; i++){
+// //         sum += arr2[i];
+// //     }
+// //     return sum; // return the total 
+// // }
+
+// // console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])); //output: 21
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function arrayPlusArray(arr1, arr2){
+//     let sum = 0;
+
+//     for(let i=0; i<arr1.length; i++){
+//         // sum += arr1[i];
+//         sum = sum + arr1[i];
+//     }
+//     for(let i=0; i<arr2.length; i++){
+//         // sum +=arr2[i];
+//         sum = sum + arr2[i];
+//     }
+//     return sum;
+// }
+// console.log(arrayPlusArray([1, 2, 3],[4, 5, 6]));
+
+
+
+function arrayPlusArray(arr1, arr2) {
+  let r = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    r += arr1[i] + arr2[i];
+  }
+  return r;
 }
 
-console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])); //output: 21
+console.log(arrayPlusArray([1, 2, 3],[4, 5, 6]));
---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,6 @@
+function makeUpperCase(str){
+    return str.toUpperCase();
+}
+
+console.log(makeUpperCase("hello world")); // HELLO WORLD
+
