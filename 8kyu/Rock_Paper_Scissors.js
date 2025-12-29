52c4d9aa1b175739e982869d7f8a545e4232d393
Komil Kurb <komilpilot@gmail.com>
2025-12-28 21:45:09 -0500

    Auto: update Codewars solutions

--- 8kyu/Rock_Paper_Scissors.js
+++ 8kyu/Rock_Paper_Scissors.js
@@ -30,14 +30,3 @@ const rps = (player1, player2) => {
     if (player2 === 'scissors') return 'Draw!';
   }
 };
-console.log(rps('rock', 'scissors'));   // Player 1 won!
-console.log(rps('scissors', 'paper'));  // Player 1 won!
-console.log(rps('paper', 'rock'));      // Player 1 won!
-
-console.log(rps('scissors', 'rock'));   // Player 2 won!
-console.log(rps('paper', 'scissors'));  // Player 2 won!
-console.log(rps('rock', 'paper'));      // Player 2 won!
-
-console.log(rps('rock', 'rock'));       // Draw!
-console.log(rps('paper', 'paper'));     // Draw!
-console.log(rps('scissors', 'scissors'));// Draw!
